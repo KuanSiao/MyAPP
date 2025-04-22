b1be9e0bd57f7e59585435d9ba02617a422e0c23
CSF-AD\kuan_siao <kuan_siao@mail.csf.org.tw>
2025-04-22 11:44:38 +0800

    主專案v1

--- MainProjct/Form1.cs
+++ MainProjct/Form1.cs
@@ -1,3 +1,5 @@
+using MyCommonLib;
+
 namespace MainProjct
 {
     public partial class Form1 : Form
@@ -9,7 +11,10 @@ namespace MainProjct
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            DateTime date = Convert.ToDateTime("1995/3/21");
+            string age = new TestMethod().GetAgeByYear(date);
+            MessageBox.Show(new TestMethod().GetString("¤p©ú"));
+            MessageBox.Show(age, "Age", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
